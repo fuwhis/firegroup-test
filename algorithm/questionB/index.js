7b3e0aa3259ab6880ccd4e07a4afd485072aa679
Quy Nguyen <steve.parker117@gmail.com>
2022-05-11 09:15:46 +0700

    update problem B

--- algorithm/questionB/index.js
+++ algorithm/questionB/index.js
@@ -1,5 +1,6 @@
 /**
  * @params {array[]} arr
+ * create a Map to store the appearance of each element in array
  */
 
 var findFrequent = (arr) => {
@@ -16,13 +17,17 @@ var findFrequent = (arr) => {
   // loop into the array
   for (let i = 0; i < arr.length; i++) {
     const element = arr[i];
-
+    // check arr[i] value equivalent with null
+    // if yes, return to 1
+    // else arr[i] increased
     if (itemMap[element] == null) {
       itemMap[element] = 1;
     } else {
       itemMap[element]++;
     }
 
+    // in loop, assign element to items when it larger than maxCount = 1, and assign maxCount with value of arr[i]
+    // if itemMap[element] == maxCount we push element to items[] and assign value to maxCount
     if (itemMap[element] > maxCount) {
       items = [element];
       maxCount = itemMap[element];
@@ -31,8 +36,10 @@ var findFrequent = (arr) => {
       maxCount = itemMap[element];
     }
   }
+  // return to items
   console.log(items);
 }
+
 findFrequent([3, 7, 3, 7, 8]);
 findFrequent([null, "hello", true, null]);
 findFrequent([false, "up", "down", "left", "right", true, false]);
