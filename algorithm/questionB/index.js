fcd279b468a01d47fd690b0ead5b44e41d779fb1
Quy Nguyen <steve.parker117@gmail.com>
2022-05-11 09:06:48 +0700

    optimize O(n^2) to O(n) solution of problem B in question 1

--- algorithm/questionB/index.js
+++ algorithm/questionB/index.js
@@ -3,22 +3,36 @@
  */
 
 var findFrequent = (arr) => {
-  var count = 1;
-  var tmp = 0;
-  var item;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; arr.length; j++) {
-      if (arr[i] === arr[j])
-        tmp++;
-      if (count < tmp) {
-        count = tmp;
-        item = arr[i];
-      }
-    }
-    tmp = 0;
+  // check array null
+  if (arr.length == 0) {
+    return null;
   }
-  console.log(item);
+
+  // create variable 
+  var itemMap = {},
+    maxCount = 1,
+    items = [];
+
+  // loop into the array
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+
+    if (itemMap[element] == null) {
+      itemMap[element] = 1;
+    } else {
+      itemMap[element]++;
+    }
+
+    if (itemMap[element] > maxCount) {
+      items = [element];
+      maxCount = itemMap[element];
+    } else if (itemMap[element] == maxCount) {
+      items.push(element);
+      maxCount = itemMap[element];
+    }
+  }
+  console.log(items);
 }
 findFrequent([3, 7, 3, 7, 8]);
-//findFrequent([null, "hello", true, null]);
-//findFrequent([false, "up", "down", "left", "right", true, false]);
+findFrequent([null, "hello", true, null]);
+findFrequent([false, "up", "down", "left", "right", true, false]);
